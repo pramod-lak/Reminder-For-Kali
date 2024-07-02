b7a141c1296bfd8c11622e0ec860289d9d610384
Pramod Lakshan <lakshanvvr@gmail.com>
2024-07-02 20:25:52 +0530

    Enhanced the script
    Added status messages and error handling to the script.

--- reminder.sh
+++ reminder.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Reminder is active & running..."
+
 #Using zenity to create a dialog box
-zenity --info --text="Check if the Nmap scan has finished." --title="Reminder" --width=300 --height=200
+zenity --info --text="Check if the Nmap scan has finished." --title="Reminder" --width=300 --height=200 2>/dev/null
 
 #Checking if the user clicked on OK
 while true; do
@@ -9,3 +11,7 @@ while true; do
         break
     fi
 done
+
+echo -e "\nExiting..."
+sleep 1
+echo -e "\nReminder ended."
