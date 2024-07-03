1bba9ad9ca22def10b891cce37b18c9295d2ff85
Pramod Lakshan <lakshanvvr@gmail.com>
2024-07-03 13:04:45 +0530

    Added music
    Music plays when the reminder notification is displayed.

--- reminder.sh
+++ reminder.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+#This is a reminder for 5 seconds
+sleep 5
+
+#Function to play the music continuously
+play_music() {
+    while true; do
+        #Add your music file path here (less than 10 seconds recommended)
+        paplay /home/user/Music/alarm-clock-elapsed.oga
+    done
+}
+
+#Play the music
+play_music &
+music_pid=$!
+
 echo "Reminder is active & running..."
 
 #Using zenity to create a dialog box
@@ -8,6 +23,9 @@ zenity_pid=$!
 
 #Wait for the zenity process to finish
 wait $zenity_pid
+
+#Kill the music process
+kill $music_pid
 
 echo -e "\nExiting..."
 sleep 1
