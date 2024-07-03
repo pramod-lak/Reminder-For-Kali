0c49991998477470050660e54d58ac487645e097
Pramod Lakshan <lakshanvvr@gmail.com>
2024-07-03 13:39:27 +0530

    Added SIGINT handler
    Used the trap command to handle the SIGINT signal.

--- reminder.sh
+++ reminder.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Reminder is active & running..."
+
+#Function to handle the SIGINT signal (Ctrl+C)
+trap 'kill $music_pid 2>/dev/null; kill $zenity_pid 2>/dev/null; echo -e "\nExiting..."; sleep 1; echo -e "\nReminder ended."; exit 0' SIGINT
+
 #This is a reminder for 5 seconds
 sleep 5
 
@@ -14,8 +19,6 @@ play_music() {
 #Play the music
 play_music &
 music_pid=$!
-
-echo "Reminder is active & running..."
 
 #Using zenity to create a dialog box
 zenity --info --text="Check if the Nmap scan has finished." --title="Reminder" --width=300 --height=200 2>/dev/null &
